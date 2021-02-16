23d5b74cb713af5d8533258dbab950d44f7eea6c
WookieFromAbove <benjmercier11@gmail.com>
2021-02-16 08:58:10 -0500

    AI following baked path.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _navMeshAgent;
+    [SerializeField]
+    private NavMeshPath _navMeshPath;
+
+    [SerializeField]
+    private GameObject _target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_navMeshAgent == null)
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        if (_target == null)
+        {
+            _target = GameObject.Find("TargetPos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_navMeshAgent.enabled)
+        {
+            MoveToTarget(_target.transform.position);
+        }
+    }
+
+    private void MoveToTarget(Vector3 target)
+    {
+        _navMeshAgent.SetDestination(target);
+        
+    }
+}
