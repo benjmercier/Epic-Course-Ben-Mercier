03ecd3d74fab0a468d3e854e9d431c760e67bba0
WookieFromAbove <benjmercier11@gmail.com>
2021-02-26 10:44:38 -0500

    Downloaded assets from Filebase and created tower placement prefabs and script.

---
+++ Assets/Mercier/Scripts/Classes/TowerPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mercier.Scripts.Classes
+{
+    public class TowerPosition : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _towerPos;
+
+        [SerializeField]
+        private bool _isActive;
+
+        public bool ReturnActiveState()
+        {
+            return _isActive;
+        }
+    }
+}
+
