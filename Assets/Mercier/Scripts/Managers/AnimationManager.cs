cbf165064b62fb48c41e3f9927c40754397dc897
WookieFromAbove <benjmercier11@gmail.com>
2021-03-13 16:33:12 -0500

    Created Firing animator mask and layer on Mech1 animation controller.

---
+++ Assets/Mercier/Scripts/Managers/AnimationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mercier.Scripts.Managers
+{
+    public class AnimationManager : MonoSingleton<AnimationManager>
+    {
+        public List<string> layers = new List<string>();
+
+        public List<string> parameters = new List<string>();
+    }
+}
+
